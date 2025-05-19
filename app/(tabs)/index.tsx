48f0a61ecc4b0f7fa69ce69cd1252ea7d0585821
Perry Hoang <hoanghuynh5555@gmail.com>
2025-05-19 11:19:37 +0700

    set up axios call api

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,42 @@
-import { t } from "../../locales";
-import { getStatusBarHeight } from "../../utils/uiHelper";
+import { useQuery } from "@tanstack/react-query";
 import * as React from "react";
-import { View, Text } from "react-native";
+import { ActivityIndicator, ScrollView, Text, View } from "react-native";
+import { postsApi } from "../../services/api/posts";
+import { getStatusBarHeight } from "../../utils/uiHelper";
 
 export default function Home() {
   const topPadding = getStatusBarHeight + 40;
 
+  // React Query hook
+  const {
+    data: posts,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["posts"],
+    queryFn: postsApi.getPosts,
+  });
+  console.log(process.env.PUBLIC_API_URL);
+
   return (
-    <View
-      className={`flex-1 bg-background`}
-      style={{ paddingTop: topPadding, backgroundColor: "red" }}
-    >
-      <View />
-      <Text className="text-2xl font-bold text-center accent-black">
-        {t("HOME")}
-      </Text>
+    <View className="flex-1 bg-background" style={{ paddingTop: topPadding }}>
+      {isLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : error ? (
+        <Text className="text-red-500 text-center">Error loading posts</Text>
+      ) : (
+        <ScrollView className="px-4">
+          {posts?.map((post: any) => (
+            <View
+              key={post.id}
+              className="bg-white p-4 rounded-lg mb-4 shadow-sm"
+            >
+              <Text className="font-bold text-lg mb-2">{post.title}</Text>
+              <Text className="text-gray-600">{post.body}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      )}
     </View>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,14 @@ import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import "../global.css";
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (__DEV__) {
   require("../config/ReactotronConfig");
   console.log("🔧 Reactotron Configured");
 }
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   const { i18n } = useTranslation();
@@ -29,20 +32,22 @@ export default function RootLayout() {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="index"
-        options={{ title: "Home", headerShown: false }}
-      />
-      <Stack.Screen
-        name="login"
-        options={{ title: "Login", headerShown: false }}
-      />
-      <Stack.Screen
-        name="register"
-        options={{ title: "Register", headerShown: false }}
-      />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="index"
+          options={{ title: "Home", headerShown: false }}
+        />
+        <Stack.Screen
+          name="login"
+          options={{ title: "Login", headerShown: false }}
+        />
+        <Stack.Screen
+          name="register"
+          options={{ title: "Register", headerShown: false }}
+        />
+      </Stack>
+    </QueryClientProvider>
   );
 }
